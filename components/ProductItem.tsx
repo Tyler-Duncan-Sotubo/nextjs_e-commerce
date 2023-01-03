f27884d243d8688df4d2b9f6b7ea774c2a330d5b
tyler-Wiz <tylertooxclusive@gmail.com>
2023-01-03 11:36:20 +0000

    Products Components

---
+++ components/ProductItem.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import React, { FC } from "react";
+
+interface Props {
+  product: {
+    slug: string;
+    name: string;
+    image: string;
+    brand: string;
+    price: number;
+  };
+}
+const ProductItem: FC<Props> = ({ product }) => {
+  return (
+    <div className="font-display mb-5">
+      <Link href={`/product/${product.slug}`}>
+        <img src={product.image} alt={product.name} className="shadow mb-4" />
+      </Link>
+      <div>
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-md mb-2">
+            {product.name} - {product.brand}
+          </h2>
+        </Link>
+        <p className="text-sm mb-2">{product.price}$</p>
+        <button type="button" className="text-violet-600 font-bold">
+          + Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
