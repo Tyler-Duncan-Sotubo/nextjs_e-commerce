0602e0e2d6a14a4c5b8e7a776da5b43a1826e917
tyler-Wiz <tylertooxclusive@gmail.com>
2023-01-08 21:19:45 +0000

    SSR

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,31 +1,21 @@
 import Layout from "../components/Layout";
 import { Slider } from "../components/Slider";
-import { db } from "../firebase";
-import { getDocs, collection } from "firebase/firestore";
-import { wrapper } from "../redux/store/store";
-import { useSelector } from "react-redux";
-import { setSlideData } from "../redux/reducer/slides";
-import ProductItem from "../components/ProductItem";
-import { getProductSelector } from "../redux/reducer/products";
-import { useGetAllProductsQuery } from "../modules/productsApi";
 import axios from "axios";
+import { wrapper } from "../redux/store/store";
+import { setProductData, getProductSelector } from "../redux/reducer/products";
+import { useSelector } from "react-redux";
+import AllProducts from "../components/AllProducts";
 
 export default function Home() {
-  const product = useSelector(getProductSelector);
-  const slidesData = product[0].slide[0];
-
-  const { data } = useGetAllProductsQuery();
+  const { products } = useSelector(getProductSelector);
+  const { product } = products;
+  const data = product.products;
 
   return (
     <>
       <Layout title="Hello">
-        <Slider allSlides={slidesData} />
-        <div className="grid grid-cols-1 gap-6 px-28 py-10 md:grid-cols-3 lg:grid-cols-3">
-          {data &&
-            data?.map((product: any) => (
-              <ProductItem product={product} key={product.slug} />
-            ))}
-        </div>
+        <Slider data={data} />
+        <AllProducts data={data} shirts={[]} />
       </Layout>
     </>
   );
@@ -33,18 +23,13 @@ export default function Home() {
 
 export const getServerSideProps = wrapper.getServerSideProps(
   (store) => async () => {
-    const data: any = [];
-    const querySnapshot = await getDocs(collection(db, "Slides"));
-    querySnapshot.forEach((lyrics) => {
-      data.push({ id: lyrics.id, ...lyrics.data() });
-    });
-    store.dispatch(setSlideData(data));
-
     const res = await axios.get("http://localhost:1000/api/products");
-    const checkData = res.data;
+    const apiData = res.data;
+
+    store.dispatch(setProductData(apiData));
 
     return {
-      props: { checkData },
+      props: {},
     };
   }
 );
