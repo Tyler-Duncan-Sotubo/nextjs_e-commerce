799e313e6ef5fff4f826c4bfb9e02847a979c151
tyler-Wiz <tylertooxclusive@gmail.com>
2023-01-11 12:33:41 +0000

    Read

--- frontend/pages/welcome.tsx
+++ frontend/pages/welcome.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "../components/Layout";
 
 const Welcome = () => {
-  return <Layout title="Check">hello</Layout>;
+  return <Layout title="Check">Welcome</Layout>;
 };
 
 export default Welcome;
